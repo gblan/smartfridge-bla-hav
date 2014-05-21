a1ef2169f708a2f28d4568293309b63be4bf45e4
guillaume.blanchard37 <guillaume.blanchard37@gmail.com>
2014-05-21 08:41:16 +0000

    Initial Import :)

---
+++ SmartFridge-Bla-Hav/src/smartfridge/actions/History.java
@@ -0,0 +1,13 @@
+package smartfridge.actions;
+
+public class History {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ SmartFridge-Bla-Hav/src/smartfridge/product/ProductPerishable.java
@@ -0,0 +1,13 @@
+package smartfridge.product;
+
+public class ProductPerishable {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
