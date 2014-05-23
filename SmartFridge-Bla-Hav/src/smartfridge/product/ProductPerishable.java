a90c5a67a1d4ec46f9f37dbfe3475ae7b4b30a29
yanickhavil <yanickhavil@gmail.com>
2014-05-23 07:44:53 +0000

    Salut

--- SmartFridge-Bla-Hav/src/smartfridge/product/ProductPerishable.java
+++ SmartFridge-Bla-Hav/src/smartfridge/product/ProductPerishable.java
@@ -22,7 +22,7 @@ public class ProductPerishable extends ProductAbstract {
 	@Override
 	public String toString() {
 		String tmp = super.toString();
-		tmp += " --- " + this.datePerishment.toString();
+		tmp += " --- " + this.datePerishment.getDate() + "/" + this.datePerishment.getMonth() + "/"+ this.datePerishment.getYear();
 
 		return tmp;
 	}
